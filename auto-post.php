ac623d7bb57481fadee078defa528aca5321c427
rafal <r.p.wojciechowski10@gmail.com>
2026-01-26 20:18:10 +0100

    initial commit

---
+++ auto-post.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+* Plugin Name: Auto post
+*/
+
+if (!defined('ABSPATH')) exit;
+
+use AutoPost\Plugin;
+
+(new Plugin)->init();
---
+++ src/Plugin.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AutoPost;
+
+class Plugin {
+    public function init() {
+        
+    }
+}
